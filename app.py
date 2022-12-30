11070a678312d42661b6d7d3672da248db66e918
Sathya Prakash <93382809+sathu08@users.noreply.github.com>
2022-12-30 13:21:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, session, request, flash, redirect,url_for
 from flask_mysqldb import MySQL
 import smtplib
 from datetime import timedelta,date
@@ -16,7 +16,38 @@ smtpObj.starttls()
 smtpObj.login('libarycollege@gmail.com', 'zwbgbvdedsyaoauq')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'email_id' in request.form and 'password' in request.form:
+        email = request.form['email_id']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute('select * from account WHERE email_id = % s AND password = % s', (email, password))
+        account = cur.fetchone()
+        cur.connection.commit()
+        cur.close()
+        if account:
+            session['loggedin'] = True
+            session['email_id'] = account[1]
+            session['password'] = account[2]
+            msg = 'Logged in successfully !'
+            return redirect(url_for('index'))
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         User_Details = request.form
@@ -51,7 +82,7 @@ def users():
     user = cur.execute(
         "SELECT email,name,book,date_book_purchased FROM user WHERE date_book_purchased >= CURRENT_DATE AND status_book = 'not submit'")
 
-    if user > 0:
+    if user >= 0:
         userDetails = cur.fetchall()
         for email, name, book, date_book_purchased in userDetails:
             body = ('Subject: Book Rent From Library Need to Return.\n\nDear %s,\nYou have rent a '
@@ -78,11 +109,12 @@ def book_return():
         cur.close()
         body = ('Subject: Book Rent From Library.\n\nDear %s,\nYou have rent a '
                 'Book from %s,Name of  the Book is %s,'
-                'Please make sure to return the book on time. \nThank you!' % (Name))
+                'Please make sure to return the book on time. \nThank you!' % Name)
         print('Sending email to %s...' % Email)
         smtpObj.sendmail('sender email', Email, body)
         smtpObj.quit()
-        return "Success"
+        flash('Submitted SuccessFully')
+        return render_template('Book_return.html')
     return render_template('Book_return.html')
 
 
@@ -95,6 +127,50 @@ def book_details():
     if user > 0:
         userDetails = cur.fetchall()
         return render_template('book_details.html', userDetails=userDetails)
+@app.route('/forget_email', methods=['GET','POST'])
+def forget_email():
+    msg=''
+    if request.method == 'POST':
+        user = request.form
+        email = user['forgot_email']
+        cur = mysql.connection.cursor()
+        cur.execute('select email_id from account')
+        cur.fetchall()
+        for i in cur:
+            if i[0] == email:
+                body = ("Hi! Did you forget your password?\n Click on this link to change your password:"
+                        '"http://127.0.0.1:5000/reset_password\n"'
+                        '"If you did not request a password reset, then simply ignore this email and no changes will be made."'
+                        '"Have a great day!"')
+                print('Sending email to %s...' % email)
+                # smtpObj.sendmail('sender email', email, body)
+                # smtpObj.quit()
+                msg= "Successfully Sent"
+                forget_email.var = email
+                return render_template('forgot_email.html',msg=msg)
+            else:
+                msg = 'Incorrect Email'
+    return render_template('forgot_email.html',msg=msg)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    msg=''
+    email = forget_email.var
+    if request.method == 'POST':
+        password = request.form['password']
+        verify_password = request.form['password_verify']
+        if password == verify_password:
+            cur = mysql.connection.cursor()
+            cur.execute('update account set password = %s where email_id=%s', (password, email))
+            cur.connection.commit()
+            cur.close()
+            return redirect(url_for('login'))
+
+        else:
+            msg = "Password don't match"
+    return render_template('reset_password.html',msg=msg)
+
 
 
 if __name__ == "__main__":
